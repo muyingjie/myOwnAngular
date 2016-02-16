306c07c2155741b1dcfd4741d25206bcdc312c72
myjAtComp <1368468125@qq.com>
2016-02-16 18:18:28 +0800

    scope P65

--- myOwnDev/myAngular.js
+++ myOwnDev/myAngular.js
@@ -3,11 +3,14 @@
         //双$符号代表是私有属性
         this.$$watchers = [];
         this.$$lastDirtyWatch = null;
+        this.$$asyncQueue = [];
+        this.$$phase = null;
     }
-    Scope.prototype.$watch = function (watchFn, listenerFn) {
+    Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
         var watcher = {
             watchFn: watchFn,
             listenerFn: listenerFn || function () { },
+            valueEq: !!valueEq,
             last: initWatchVal
         };
         this.$$watchers.push(watcher);
@@ -21,9 +24,11 @@
         _.forEach(this.$$watchers, function (watcher) {
             newValue = watcher.watchFn(self);
             oldValue = watcher.last;
-            if (newValue !== oldValue) {
+            //if (newValue !== oldValue) {
+            if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
                 self.$$lastDirtyWatch = watcher;
-                watcher.last = newValue;
+                //watcher.last = newValue;
+                watcher.last = watcher.valueEq ? _.cloneDeep(newValue) : newValue;
                 watcher.listenerFn(
                     newValue,
                     (oldValue == initWatchVal ? newValue : oldValue),
@@ -40,12 +45,58 @@
         var ttl = 10;//ttl means Time To Live
         var dirty;
         this.$$lastDirtyWatch = null;
+        this.$beginPhase("$digest");
         do {
+            while (this.$$asyncQueue.length) {
+                var asyncTask = this.$$asyncQueue.shift();
+                asyncTask.scope.$eval(asyncTask.expression);
+            }
             dirty = this.$$digestOnce();
-            if (dirty && !(ttl--)) {
+            if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
+                this.$clearPhase();
                 throw "10 digest iterations reached";
             }
-        } while (dirty);
+        } while (dirty || this.$$asyncQueue.length);
+        this.$clearPhase();
+    };
+    Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+        if (valueEq) {
+            return _.isEqual(newValue, oldValue);
+        } else {
+            return newValue === oldValue || (
+                typeof newValue === "number" &&
+                typeof oldValue === "number" &&
+                isNaN(newValue) &&
+                isNaN(oldValue)
+            );
+        }
+    };
+    Scope.prototype.$eval = function (expr, locals) {
+        return expr(this, locals);
+    };
+    Scope.prototype.$apply = function (expr) {
+        try {
+            this.$beginPhase("$apply");
+            return this.$eval(expr);
+        } finally {
+            this.$clearPhase();
+            this.$digest();
+        }
+    };
+    Scope.prototype.$evalAsync = function (expr) {
+        this.$$asyncQueue.push({
+            scope: this,
+            expression: expr
+        });
+    };
+    Scope.prototype.$beginPhase = function (phase) {
+        if (this.$$phase) {
+            throw this.$$phase + " already in progress";
+        }
+        this.$$phase = phase;
+    };
+    Scope.prototype.$clearPhase = function () {
+        this.$$phase = null;
     };
 
     function initWatchVal() { }
